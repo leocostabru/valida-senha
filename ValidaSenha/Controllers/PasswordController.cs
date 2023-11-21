df71389d86751edbdb9d74f7922cac1cbe7dbd88
Leonardo Costae <leo.costa.bru@hotmail.com>
2023-11-21 16:43:11 -0300

    atualiza codigo

--- ValidaSenha/Controllers/PasswordController.cs
+++ ValidaSenha/Controllers/PasswordController.cs
@@ -8,45 +8,33 @@ namespace ValidaSenha.Controllers;
 public class PasswordController : ControllerBase
 {
     [HttpPost("password")]
-    public IActionResult ValidatePassword([FromBody] PasswordDto passwordDto)
+    public IActionResult ValidatePassword([FromBody] PasswordDto password)
     {
-        IActionResult isValidPassword = PasswordValidator(passwordDto.Password); 
-
-        if (isValidPassword is OkResult)
-        {
-            return Ok(new { Message = "Senha válida!" });
-        }
-
-        return BadRequest(new { Message = "Senha invalida"});
-    }
-
-    private IActionResult PasswordValidator(string password)
-    {
-        if (password.Length < 6)
+        if (password.Password.Length < 6)
         {
             return BadRequest(new { Message = "A senha deve conter 6 ou mais caracteres."});
         }
 
-        if (!password.Any(char.IsUpper))
+        if (!password.Password.Any(char.IsUpper))
         {
             return BadRequest(new { Message = "A senha deve conter pelo menos uma letra maiuscula."});
         }
         
-        if (!password.Any(char.IsLower))
+        if (!password.Password.Any(char.IsLower))
         {
             return BadRequest(new { Message = "A senha deve conter pelo menos 1 letra minuscula."});
         }
         
-        if (!password.Any(char.IsDigit))
+        if (!password.Password.Any(char.IsDigit))
         {
             return BadRequest(new { Message = "A senha deve conter pelo menos 1 numero."});
         }
         
-        if(!password.Any(c => "*!_?".Contains(c)))
+        if(!password.Password.Any(c => "*!_?".Contains(c)))
         {
             return BadRequest(new { Message = "A senha deve conter pelo menos 1 caracter especial (*!_?)."});
         }
 
-        return Ok(password);
+        return Ok(new {Message = "Senha criada com sucesso!"});
     }
 }
